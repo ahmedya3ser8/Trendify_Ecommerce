1a42702e1ad3cb35a89a6fe4c63a8b44b276d2bb
ahmed yasser <ahmedya3ser8@gmail.com>
2025-11-02 20:06:44 +0200

    finaliz blog page

--- src/app/features/blog/blog.component.ts
+++ src/app/features/blog/blog.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
-
+  blogList = signal([
+    {
+      image: '/images/blogs/blog-1.png',
+      category: 'Fashion',
+      date: new Date(),
+      title: 'The perfect Shopify theme',
+      description: 'Shopify is the second most popular eCommerce store builder in the world. You can use Shopify in...'
+    },
+    {
+      image: '/images/blogs/blog-2.png',
+      category: 'Fashion',
+      date: new Date(),
+      title: 'The perfect Shopify theme',
+      description: 'Shopify is the second most popular eCommerce store builder in the world. You can use Shopify in...'
+    },
+    {
+      image: '/images/blogs/blog-3.png',
+      category: 'Fashion',
+      date: new Date(),
+      title: 'The perfect Shopify theme',
+      description: 'Shopify is the second most popular eCommerce store builder in the world. You can use Shopify in...'
+    },
+    {
+      image: '/images/blogs/blog-4.png',
+      category: 'Fashion',
+      date: new Date(),
+      title: 'The perfect Shopify theme',
+      description: 'Shopify is the second most popular eCommerce store builder in the world. You can use Shopify in...'
+    },
+    {
+      image: '/images/blogs/blog-5.png',
+      category: 'Fashion',
+      date: new Date(),
+      title: 'The perfect Shopify theme',
+      description: 'Shopify is the second most popular eCommerce store builder in the world. You can use Shopify in...'
+    },
+    {
+      image: '/images/blogs/blog-6.png',
+      category: 'Fashion',
+      date: new Date(),
+      title: 'The perfect Shopify theme',
+      description: 'Shopify is the second most popular eCommerce store builder in the world. You can use Shopify in...'
+    }
+  ])
 }
