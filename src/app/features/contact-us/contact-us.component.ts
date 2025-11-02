68ebe5aba3dcc8487c49bba76e8b64d8c3639a46
ahmed yasser <ahmedya3ser8@gmail.com>
2025-11-02 21:26:52 +0200

    finaliz contact-us page

--- src/app/features/contact-us/contact-us.component.ts
+++ src/app/features/contact-us/contact-us.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { Select } from 'primeng/select';
+import { FloatLabel } from "primeng/floatlabel"
+import { SubscriptionComponent } from "@shared/components/subscription/subscription.component";
+
+interface ITopic {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [Select, FloatLabel, ReactiveFormsModule, SubscriptionComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
-export class ContactUsComponent {
-
+export class ContactUsComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+  topics: ITopic[] = [];
+  form!: FormGroup;
+  ngOnInit(): void {
+    this.initTopics();
+    this.initForm();
+  }
+  initTopics(): void {
+    this.topics = [
+      { name: 'select one', code: 'option1' },
+      { name: 'select two', code: 'option2' },
+      { name: 'select three', code: 'option3' }
+    ];
+  }
+  initForm(): void {
+    this.form = this.fb.group({
+      firstName: [null, [Validators.required]],
+      lastName: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      phoneNumber: [null, [Validators.required]],
+      topic: [null, [Validators.required]],
+      message: [null, [Validators.required]],
+      acceptTerms: [false, [Validators.requiredTrue]]
+    })
+  }
+  submitForm(): void {
+    console.log(this.form.value);
+  }
 }
---
+++ src/app/shared/components/basic-input/basic-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-input',
+  imports: [],
+  templateUrl: './basic-input.component.html',
+  styleUrl: './basic-input.component.css'
+})
+export class BasicInputComponent {
+
+}
